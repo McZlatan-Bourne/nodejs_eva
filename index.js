87163f80aa5a3039058c3855fa5057dd0ca53e55
McZlatan Bourne <mczlatan@localhost.localdomain>
2016-05-18 16:29:57 +0100

    configured views with nunjucks for rendering stuffs

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false})
 
+var path = require('path');
+var nunjucks = require('nunjucks');
+var nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, '/views' ) ));
+nunjucksEnv.express( app );
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/" + "index.html");
+    res.render("index.html");
 })
+
+app.get('/events', function(req, res) {
+  res.render('events.html', {
+    title : 'E.V.A rendering with nunjucks',
+    events : [
+      { name : 'Event #1' },
+      { name : 'Event #2' },
+      { name : 'Event #3' },
+      { name : 'Event #4' },
+    ]
+  });
+});
 
 var server = app.listen(8081, function () {
 var host = server.address().address
